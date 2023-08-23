2d10ab2d66cdb9204f868f9531a90b315a0f0f37
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-23 21:40:07 +0000

    Update error_print_funcs.c

--- error_print_funcs.c
+++ error_print_funcs.c
@@ -23,3 +23,19 @@ void file_open_err(const char *filename)
 {
 	fprintf(stderr, "Error: Can't open file %s\n", filename);
 }
+
+
+/**
+ * unknown_instruc_err - Prints error message to stderr
+ * when instruction is unknown
+ * @line_number: file line number when reading
+ * @read_opcode: read command from file to execute
+ */
+void unknown_instruc_err(unsigned int line_number,
+	char *read_opcode)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n",
+		line_number, read_opcode);
+}
+
+
