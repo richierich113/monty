33ce0c3942c5aa97a115c33cd58550927737c004
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-23 14:08:07 +0000

    Update error_prnts.c

--- error_prnts.c
+++ error_prnts.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * usage_error - Prints error message to stderr
+ * argnum_error - Prints error message to stderr
  * when argement passed is not 2
  * Return: (EXIT_FAILURE) always.
  */
-int usage_error(void)
+int argnum_error(void)
 {
 	fprintf(stderr, "USAGE: monty file\n");
 	return (EXIT_FAILURE);
