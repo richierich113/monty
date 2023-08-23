4d3b7f25215ad3af7c813cf4f8041a0c5442da76
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-23 22:09:07 +0000

    Update monty.h

--- monty.h
+++ monty.h
@@ -80,9 +80,8 @@ extern global_glob_data_struct glob_data;
 /* new functions*/
 int argnum_error(void);
 void file_open_err(const char *filename);
-/**void unknown_instruc_err(unsigned int glob_data->line_number,
- *	char *read_opcode);
- */
+void unknown_instruc_err(unsigned int line_number,
+	char *read_opcode);
 
 
 int start_global_glob_data_struct(global_glob_data_struct *glob_data);
