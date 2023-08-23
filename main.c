aac100124c229b98f7574caadda5d6641cbd2c92
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-23 16:17:25 +0000

    Update main.c

--- main.c
+++ main.c
@@ -4,47 +4,44 @@ vars var;
 
 /**
  * main - Start LIFO, FILO program
- * @argc: Number of arguments
- * @argv: Pointer containing arguments
+ * @ac: Number of arguments
+ * @av: Pointer containing arguments
  * Return: 0 Success, 1 Failed
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	char *opcode;
-	int check, arg_num = 2;
 
-	if (argc < arg_num || argc > arg_num)
+	if (ac != 2)
 	{
-		return (argnum_error());
-	}
-	check = start_vars(&var);
-	if (check != 0)
+		fprintf(stderr, "USAGE: monty file\n");
 		return (EXIT_FAILURE);
-	if (access(filename, R_OK) == 0)
+	}
+
+	if (start_vars(&var) != 0)
+		return (EXIT_FAILURE);
+
+	var.file = fopen(av[1], "r");
+	if (!var.file)
 	{
-		var.file = fopen(argv[1], "r");
-		if (!var.file)
-		{
-			file_open_err(argv[1]);
-			free_all();
-			return (EXIT_FAILURE);
-		}
-		while (getline(&var.buff, &var.tmp, var.file) != -1)
-		{
-			opcode = strtok(var.buff, " \r\t\n");
-			if (opcode != NULL)
-				if (call_funct(&var, opcode) == EXIT_FAILURE)
-				{
-					free_all();
-					return (EXIT_FAILURE);
-				}
-			var.line_number++;
-		}
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		free_all();
-		return (EXIT_SUCCESS);
-	}
-	else
-	{
-		file_open_err(argv[1]);
 		return (EXIT_FAILURE);
+	}
+
+	while (getline(&var.buff, &var.tmp, var.file) != EOF)
+	{
+		opcode = strtok(var.buff, " \r\t\n");
+		if (opcode != NULL)
+			if (call_funct(&var, opcode) == EXIT_FAILURE)
+			{
+				free_all();
+				return (EXIT_FAILURE);
+			}
+		var.line_number++;
+	}
+
+	free_all();
+
+	return (EXIT_SUCCESS);
 }
