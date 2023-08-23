d3ece0f2ab9a86726f5027a62bcffb05af8bd132
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-23 15:04:40 +0000

    Create error_print_funcs.c

---
+++ error_print_funcs.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * argnum_error - Prints error message to stderr
+ * when argument passed is not 2
+ * @isfile_passed: boolean value parameter
+ */
+void argnum_error(bool isfile_passed)
+{
+	if (isfile_passed)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+	}
+}
+
+
+/**
+ * file_open_err - Prints error message to stderr
+ * when argument file passed cannot open or grants
+ * no read permission
+ * @argv: argument file name passed
+ */
+void file_open_err(argv[1])
+{
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+}
