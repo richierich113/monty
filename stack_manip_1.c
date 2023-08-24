a73be060de8cebb41e92fea5d6a81f80e54e1214
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 16:10:47 +0000

    Update and rename opfun2.c to stack_manip_1.c

--- stack_manip_1.c
+++ stack_manip_1.c
@@ -1,33 +1,6 @@
 #include "monty.h"
 
 /**
- * swap - swaps the top two elements of the stack.
- * @head: Double linked list data struct head
- * @line_number: File line number when reading
- */
-void swap(stack_t **head, unsigned int line_number)
-{
-	int val_a;
-	int val_b;
-
-	if (!*head || !(*head)->next)
-	{
-		swap_err(line_number);
-
-		free_all();
-
-		exit(EXIT_FAILURE);
-	}
-
-	val_a = (*head)->n;
-	val_b = (*head)->next->n;
-
-	(*head)->n = val_b;
-	(*head)->next->n = val_a;
-
-}
-
-/**
  * add - adds the top two elements of the stack.
  * @head: Double linked list data struct head
  * @line_number: File line number when reading
@@ -48,32 +21,6 @@ void add(stack_t **head, unsigned int line_number)
 	}
 
 	(*head)->next->n += (*head)->n;
-
-	pop(head, line_number);
-}
-
-/**
- * sub - subtracts the top element of the stack from the second
- * top element of the stack.
- * @head: Double linked list data struct head
- * @line_number: File line number when reading
- * Description: subtracts the top element of the stack from the second
- * top element of the stack. Store in the second top element of the stack,
- * and the top element is removed, so that at the end:
- * The top element of the stack contains the result
- * The stack is one element shorter
- */
-void sub(stack_t **head, unsigned int line_number)
-{
-	if (!*head || !(*head)->next)
-	{
-		sub_err(line_number);
-
-		free_all();
-		exit(EXIT_FAILURE);
-	}
-
-	(*head)->next->n -= (*head)->n;
 
 	pop(head, line_number);
 }
@@ -105,6 +52,63 @@ void divi(stack_t **head, unsigned int line_number)
 	}
 
 	(*head)->next->n /= (*head)->n;
+
+	pop(head, line_number);
+}
+
+
+/**
+ * sub - subtracts the top element of the stack from the second
+ * top element of the stack.
+ * @head: Double linked list data struct head
+ * @line_number: File line number when reading
+ * Description: subtracts the top element of the stack from the second
+ * top element of the stack. Store in the second top element of the stack,
+ * and the top element is removed, so that at the end:
+ * The top element of the stack contains the result
+ * The stack is one element shorter
+ */
+
+/**
+ * swap - swaps the top two elements of the stack.
+ * @head: Double linked list data struct head
+ * @line_number: File line number when reading
+ */
+void swap(stack_t **head, unsigned int line_number)
+{
+	int val_a;
+	int val_b;
+
+	if (!*head || !(*head)->next)
+	{
+		swap_err(line_number);
+
+		free_all();
+
+		exit(EXIT_FAILURE);
+	}
+
+	val_a = (*head)->n;
+	val_b = (*head)->next->n;
+
+	(*head)->n = val_b;
+	(*head)->next->n = val_a;
+
+}
+
+
+
+void sub(stack_t **head, unsigned int line_number)
+{
+	if (!*head || !(*head)->next)
+	{
+		sub_err(line_number);
+
+		free_all();
+		exit(EXIT_FAILURE);
+	}
+
+	(*head)->next->n -= (*head)->n;
 
 	pop(head, line_number);
 }
