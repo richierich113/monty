ebf86ceb36a564b5e0f6c30ef854ca59e7c4ed1b
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 16:08:22 +0000

    Update and rename opfun.c to print_stack.c

--- print_stack.c
+++ print_stack.c
@@ -43,35 +43,3 @@ void pall(stack_t **head, unsigned int line_number)
 		node_check = node_check->next;
 	}
 }
-
-
-
-/**
-* pop - removes the top element of the stack updating the pointers
-* of adjacent nodes and freeing the memory of the removed element
-* @head: Doubly linked list data struct head
-* @line_number: File line number when reading
-*/
-void pop(stack_t **head, unsigned int line_number)
-{
-	stack_t *top_node;
-
-	if (!*head)
-	{
-		pop_err(line_number);
-
-		free_all();
-
-		exit(EXIT_FAILURE);
-	}
-
-	top_node = *head;
-	*head = top_node->next;
-
-	if (top_node->next)
-	{
-		top_node->next->prev = NULL;
-	}
-
-	free(top_node);
-}
