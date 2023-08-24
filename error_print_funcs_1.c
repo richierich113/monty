aadc80caf2b4f29b1db1399a8d8ab3f454ea1fd0
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 02:13:32 +0000

    Update error_print_funcs_1.c

--- error_print_funcs_1.c
+++ error_print_funcs_1.c
@@ -5,7 +5,7 @@
  * when stack is empty
  * @line_number: file line number when reading
  */
-void pint_err(unsigned int line_number);
+void pint_err(unsigned int line_number)
 {
 	fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 }
@@ -15,7 +15,7 @@ void pint_err(unsigned int line_number);
  * pop_err - Prints error message to stderr when popping fails
  * due to empty stack
  */
-void pop_err(unsigned int line_number);
+void pop_err(unsigned int line_number)
 {
 	fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 }
