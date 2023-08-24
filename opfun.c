cf02a2aa387708d8e56be8b5eed34a392d69b3ff
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 01:16:21 +0000

    Update opfun.c

--- opfun.c
+++ opfun.c
@@ -69,18 +69,18 @@ void push(stack_t **head, unsigned int line_number)
 
 /**
  * pint - Print last node
- * @stack: Double linked list
- * @line_number: File line execution
+  * @head: Double linked list data structure
+ * @line_number: File line number when reading
  */
-void pint(stack_t **stack, unsigned int line_number)
+void pint(stack_t **head, unsigned int line_number)
 {
-	if (!*stack)
+	if (!*head)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		free_all();
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", (*stack)->n);
+	printf("%d\n", (*head)->n);
 }
 
 /**
