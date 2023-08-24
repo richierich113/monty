c46b9b65c14b4384d285c512a4b55515bbc82fe4
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 20:48:01 +0000

    Create free_alloc_memories.c

---
+++ free_alloc_memories.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+
+
+/**
+ * free_all - frees all allocated data stored in
+ * glob_data data struct global variables
+ */
+void free_all(void)
+{
+	if (glob_data.buff != NULL)
+		free(glob_data.buff);
+
+	if (glob_data.file != NULL)
+		fclose(glob_data.file);
+
+	free(glob_data.dict);
+	if (glob_data.head != NULL)
+	{
+		while (glob_data.head->next != NULL)
+		{
+			glob_data.head = glob_data.head->next;
+			free(glob_data.head->prev);
+		}
+
+		free(glob_data.head);
+	}
+}
