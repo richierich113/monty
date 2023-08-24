e96dce946629c704f60ffb38c113f630a14d09d3
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 13:46:46 +0000

    Update error_print_funcs_1.c

--- error_print_funcs_1.c
+++ error_print_funcs_1.c
@@ -20,3 +20,35 @@ void pop_err(unsigned int line_number)
 {
 	fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 }
+
+/**
+ * modulus_err - Prints error message to stderr
+ * when stack is empty
+ * @line_number: file line number when reading
+ */
+void modulus_err(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't mod, stack too short\n",
+		line_number);
+}
+
+/**
+ * print_char_err - Prints error message to stderr
+ * when stack is empty
+ * @line_number: file line number when reading
+ */
+void print_char_err(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+}
+
+
+/**
+ * printch_out_of_range_err - Prints error message to stderr
+ * If the value is not in the ascii table 
+ * @line_number: file line number when reading
+ */
+void printch_out_of_range_err(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+}
