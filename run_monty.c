c527d2be9ffe7377c6d5dad2e5ab3be1536fdb4a
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 20:46:15 +0000

    Update run_monty.c

--- run_monty.c
+++ run_monty.c
@@ -90,30 +90,7 @@ int call_funct(global_glob_data_struct *glob_data, char *read_opcode)
 	return (EXIT_SUCCESS);
 }
 
-/**
- * free_all - frees all allocated data stored in
- * glob_data data struct global variables
- */
-void free_all(void)
-{
-	if (glob_data.buff != NULL)
-		free(glob_data.buff);
 
-	if (glob_data.file != NULL)
-		fclose(glob_data.file);
-
-	free(glob_data.dict);
-	if (glob_data.head != NULL)
-	{
-		while (glob_data.head->next != NULL)
-		{
-			glob_data.head = glob_data.head->next;
-			free(glob_data.head->prev);
-		}
-
-		free(glob_data.head);
-	}
-}
 
 /**
  * _isdigit - checks if a string parameter is an integer
