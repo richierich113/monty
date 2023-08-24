e6c6b22b7c8c1bd2d8fdee83ba48fd682eecffaa
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 14:10:23 +0000

    Create error_print_funcs_3.c

---
+++ error_print_funcs_3.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+
+/**
+ * multiply_err - Prints error message to stderr
+ * when stack is empty
+ * @line_number: file line number when reading
+ */
+void multiply_err(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: can't mul, stack too short\n",
+		line_number);
+}
