23707fd39e3215594fce2fef7caa667b523b6541
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 02:48:35 +0000

    Update opfun2.c

--- opfun2.c
+++ opfun2.c
@@ -1,24 +1,28 @@
 #include "monty.h"
 
 /**
- * swap - Delete top of list
- * @stack: Double linked list
- * @line_number: File line execution
+ * swap - swaps the top two elements of the stack.
+ * @head: Double linked list data struct head
+ * @line_number: File line number when reading
  */
-void swap(stack_t **stack, unsigned int line_number)
+void swap(stack_t **head, unsigned int line_number)
 {
 	int tmp;
 
-	if (!*stack || !(*stack)->next)
+	if (!*head || !(*head)->next)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n",
 			line_number);
+
 		free_all();
+
 		exit(EXIT_FAILURE);
 	}
-	tmp = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = tmp;
+
+	tmp = (*head)->n;
+	(*head)->n = (*head)->next->n;
+
+	(*head)->next->n = tmp;
 }
 
 /**
