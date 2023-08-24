0569b6d00d25f26160ad9eba89aaa51230918faa
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-24 16:50:01 +0000

    Update change_mode.c

--- change_mode.c
+++ change_mode.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * stack - sets the format of the data to a stack (LIFO)
+ * stack_func - sets the format of the data to a stack (LIFO)
  * @head: Double linked list data struct
  * @line_number: File line number when reading
  */
-void stack(stack_t **head, unsigned int line_number)
+void stack_func(stack_t **head, unsigned int line_number)
 {
 	(void) line_number;
 	(void) head;
@@ -14,11 +14,11 @@ void stack(stack_t **head, unsigned int line_number)
 }
 
 /**
- * queue - sets the format of the data to a queue (FIFO)
+ * queue_func - sets the format of the data to a queue (FIFO)
  * @head: Double linked list data struct
  * @line_number: File line number when reading
  */
-void queue(stack_t **head, unsigned int line_number)
+void queue_func(stack_t **head, unsigned int line_number)
 {
 	(void) line_number;
 	(void) head;
